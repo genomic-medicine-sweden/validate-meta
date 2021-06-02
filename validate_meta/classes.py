5baf1415451096703a35549fb109edc4cb209b7f
parla <1q2w#E4r5t6y>
2021-06-02 08:12:47 +0200

    remove superfluous methods

--- validate_meta/classes.py
+++ validate_meta/classes.py
@@ -19,60 +19,35 @@ class DataFrameValidator:
         for field_name in definition['fields']:
             validators = []
             if 'distinct' in definition['fields'][field_name]:
-                validators.append(self.mk_distinct_validator())
+                validators.append(IsDistinctValidation())
             if 'pattern' in definition['fields'][field_name]:
-                validators.append(self.mk_pattern_validator(definition['fields'][field_name]['pattern']))
+                validators.append(MatchesPatternValidation(definition['fields'][field_name]['pattern']))
             if 'list' in definition['fields'][field_name]:
-                validators.append(self.mk_list_validator(definition['fields'][field_name]['list']))
+                validators.append(InListValidation(definition['fields'][field_name]['list']))
             if 'range' in definition['fields'][field_name]:
-                validators.append(self.mk_list_validator(definition['fields'][field_name]['range']))
+                validators.append(InRangeValidation(definition['fields'][field_name]['range']))
 
             columns.append(Column(field_name, validators))
 
         return Schema(columns)
 
-    @staticmethod
-    def mk_distinct_validator():
-        return IsDistinctValidation()
-
-    @staticmethod
-    def mk_pattern_validator(pat):
-        return MatchesPatternValidation(pat)
-
-    @staticmethod
-    def mk_list_validator(lst):
-        return InListValidation(lst)
-
-    @staticmethod
-    def mk_range_validator(rng):
-        return InRangeValidation(rng[0], rng[1])
+    # @staticmethod
+    # def mk_distinct_validator():
+    #     return IsDistinctValidation()
+    #
+    # @staticmethod
+    # def mk_pattern_validator(pat):
+    #     return MatchesPatternValidation(pat)
+    #
+    # @staticmethod
+    # def mk_list_validator(lst):
+    #     return InListValidation(lst)
+    #
+    # @staticmethod
+    # def mk_range_validator(rng):
+    #     return InRangeValidation(rng[0], rng[1])
 
     def validate(self):
         return self.schema.validate(self.df)
 
 
-def main():
-    d = {'name': ['kurt', 'urban'], 'uid': ['U1238', 'U234']}
-    df = pd.DataFrame(d)
-
-    definition = {
-        'fields': {
-            'name': {
-                'distinct': True,
-                'pattern': r'\d{4}[A-Z]{4}'
-            },
-            'uid': {
-                'distinct': True,
-                'pattern': r'U\d{4}'
-            }
-        }
-    }
-
-    v = DataFrameValidator(df, definition)
-    errors = v.validate()
-    for e in errors:
-        print(e)
-
-
-if __name__ == '__main__':
-    main()
