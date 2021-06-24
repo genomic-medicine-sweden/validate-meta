f6bafa6cdeb3a9d5bc202b856ef70fa592157840
parla <1q2w#E4r5t6y>
2021-06-24 15:22:10 +0200

    Fixed issue with DateFormatSanityValidation class

--- validate_meta/classes.py
+++ validate_meta/classes.py
@@ -19,6 +19,7 @@ class DataFrameValidator:
         columns = []
 
         for field_name in definition['fields']:
+            print(field_name)
             validators = []
             if 'lead_trail_whitespace' in definition['fields'][field_name]:
                 validators.append(LeadingWhitespaceValidation())
@@ -38,8 +39,8 @@ class DataFrameValidator:
                                                     definition['fields'][field_name]['range']['end']))
 
             if 'sane_date_pattern' in definition['fields'][field_name]:
-                validators.append(DateFormatSanityValidation(definition['fields'][field_name]['pattern'],
-                                                             definition['fields'][field_name]['sane_start']))
+                validators.append(DateFormatSanityValidation(definition['fields'][field_name]['sane_date_pattern']['pattern'],
+                                                             definition['fields'][field_name]['sane_date_pattern']['sane_start']))
 
             columns.append(Column(field_name, validators))
 
@@ -55,38 +56,33 @@ class DateFormatSanityValidation(DateFormatValidation):
     and within a sane timeframe
     """
 
-    def __init__(self, date_format: str, interval_start_val: str, **kwargs):
+    def __init__(self, date_format: str, sane_start: str, **kwargs):
         """
         :param date_format: The date format string to validate the column against. Refer to the date format code
             documentation at https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior for a full
             list of format codes
         """
         self.date_format = date_format
-        self.interval_start_val = datetime.datetime.strptime(interval_start_val, self.date_format)
+        _datetime_start = datetime.datetime.strptime(sane_start, self.date_format)
+        self.interval_start_val = _datetime_start.date()
         self.interval_end_val = datetime.date.today()
-        super().__init__(**kwargs)
+        super().__init__(self.date_format, **kwargs)
 
     @property
     def default_message(self):
         return 'Invalid date format or outside valid interval "{}"'.format(self.date_format)
 
     def valid_date(self, val):
-        sample_date = datetime.date.today()
         try:
             sample_date = datetime.datetime.strptime(val, self.date_format)
-            format_ok = True
+            sample_date = sample_date.date()
         except:
-            format_ok = False
+            return False
 
-        if self.interval_start_val <= sample_date <= self.interval_end_val:
-            sane_ok = True
-        else:
-            sane_ok = False
+        if not self.interval_start_val <= sample_date <= self.interval_end_val:
+            return False
 
-        if format_ok and sane_ok:
-            return True
-
-        return False
+        return True
 
     def validate(self, series: pd.Series) -> pd.Series:
         return series.astype(str).apply(self.valid_date)
