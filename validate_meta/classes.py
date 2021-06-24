a9b75424390f63d58c1f2eaa7cbd836897e77bef
parla <1q2w#E4r5t6y>
2021-06-24 12:16:35 +0200

    Added DateFormatSanityValidation class to support sanity checks for dates Added translation yaml Modified definition yaml

--- validate_meta/classes.py
+++ validate_meta/classes.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from pandas_schema import Column, Schema
-from pandas_schema.validation import LeadingWhitespaceValidation, \
-    TrailingWhitespaceValidation, MatchesPatternValidation, InRangeValidation, InListValidation, IsDistinctValidation
+from pandas_schema.validation import LeadingWhitespaceValidation, CustomElementValidation, \
+    TrailingWhitespaceValidation, MatchesPatternValidation, InRangeValidation, InListValidation, IsDistinctValidation, \
+    DateFormatValidation
+import datetime
 
 
 class DataFrameValidator:
@@ -26,28 +28,55 @@ class DataFrameValidator:
                 validators.append(InListValidation(definition['fields'][field_name]['list']))
             if 'range' in definition['fields'][field_name]:
                 validators.append(InRangeValidation(definition['fields'][field_name]['range']))
+            if 'date_sanity' in definition['fields'][field_name]:
+                validators.append(DateFormatSanityValidation(definition['fields'][field_name]['interval_start_date']))
 
             columns.append(Column(field_name, validators))
 
         return Schema(columns)
 
-    # @staticmethod
-    # def mk_distinct_validator():
-    #     return IsDistinctValidation()
-    #
-    # @staticmethod
-    # def mk_pattern_validator(pat):
-    #     return MatchesPatternValidation(pat)
-    #
-    # @staticmethod
-    # def mk_list_validator(lst):
-    #     return InListValidation(lst)
-    #
-    # @staticmethod
-    # def mk_range_validator(rng):
-    #     return InRangeValidation(rng[0], rng[1])
-
     def validate(self):
         return self.schema.validate(self.df)
 
 
+class DateFormatSanityValidation(DateFormatValidation):
+    """
+    Checks that each element in this column is a valid date according to a provided format string
+    and within a sane timeframe
+    """
+
+    def __init__(self, date_format: str, interval_start_val: str, **kwargs):
+        """
+        :param date_format: The date format string to validate the column against. Refer to the date format code
+            documentation at https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior for a full
+            list of format codes
+        """
+        self.date_format = date_format
+        self.interval_start_val = datetime.datetime.strptime(interval_start_val, self.date_format)
+        self.interval_end_val = datetime.date.today()
+        super().__init__(**kwargs)
+
+    @property
+    def default_message(self):
+        return 'Invalid date format or outside valid interval "{}"'.format(self.date_format)
+
+    def valid_date(self, val):
+        sample_date = datetime.date.today()
+        try:
+            sample_date = datetime.datetime.strptime(val, self.date_format)
+            format_ok = True
+        except:
+            format_ok = False
+
+        if self.interval_start_val <= sample_date <= self.interval_end_val:
+            sane_ok = True
+        else:
+            sane_ok = False
+
+        if format_ok and sane_ok:
+            return True
+        else:
+            return False
+
+    def validate(self, series: pd.Series) -> pd.Series:
+        return series.astype(str).apply(self.valid_date)
